37a6c26fa43b7262d888d40f7f95b4c5b8c5c1e7
joh liu <liuheng@chinayasha.com>
2019-06-25 17:31:29 +0800

    http网络请求

--- lib/Demo3_http.dart
+++ lib/Demo3_http.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-import 'utils/LayoutPadding.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'model/ArticRes.dart';
 
 ///
 /// http请求
@@ -16,6 +19,17 @@ class HttpDemo extends StatefulWidget {
 }
 
 class _HttpState extends State<HttpDemo> {
+  List items = List();
+
+  int pager = 0;
+
+  @override
+  Future initState() {
+    // TODO: implement initState
+    super.initState();
+    _pullNet1(pager);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,13 +37,45 @@ class _HttpState extends State<HttpDemo> {
         title: Text('http请求'),
         centerTitle: true,
       ),
-      body: LayoutPadding.Layout(
-        ListView(
-          children: <Widget>[
-
-          ],
-        ),
-      ),
+      body: ListView(
+          children: items.map((item) {
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListTile(
+              title: Text(
+                item["title"],
+                maxLines: 1,
+              ),
+              subtitle: Text(
+                item["desc"],
+                maxLines: 3,
+              ),
+              trailing: Image.network(
+                item["envelopePic"],
+                height: 100,
+              ),
+            ),
+          ),
+          elevation: 3.0,
+          margin: const EdgeInsets.all(10.0),
+        );
+      }).toList()),
     );
+  }
+
+  //获取数据
+  _pullNet1(int pager) {
+    http
+        .get("https://www.wanandroid.com/project/list/$pager/json?cid=294")
+        .then((http.Response response) {
+      var convertDataToJson = json.decode(response.body);
+      convertDataToJson = convertDataToJson["data"]["datas"];
+      //打印请求的结果
+      print(convertDataToJson);
+      setState(() {
+        items = convertDataToJson;
+      });
+    });
   }
 }
---
+++ lib/model/ArticRes.dart
@@ -0,0 +1,11 @@
+
+class ArticRes  {
+  String desc;
+  String envelopePic;
+  String title;
+
+  ArticRes(this.desc, this.envelopePic, this.title);
+}
+
+
+
